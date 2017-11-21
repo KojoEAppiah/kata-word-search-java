f4b09adf933c75ecfbc7fa0fbf60e8df6f2226d4
Kojo <kojo.e.appiah@gmail.com>
2017-11-21 11:45:16 -0500

    Finished Word List ReadIn Properly Test

--- src/main/java/WordSearcher.java
+++ src/main/java/WordSearcher.java
@@ -1,2 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class WordSearcher {
+
+    String[] wordList;
+
+    public WordSearcher(String searchFile){
+
+        try {
+            FileReader fileReader = new FileReader(searchFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            this.wordList = bufferedReader.readLine().split(",");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public String[] getWordList() {
+        return wordList;
+    }
 }
--- src/test/java/WordSearchTests.java
+++ src/test/java/WordSearchTests.java
@@ -1,2 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class WordSearchTests {
+
+    @Test
+    public void WordListReadInProperly(){
+        WordSearcher wordSearcher = new WordSearcher("../../StarTrekWordSearch.txt");
+        boolean allWordsFound = true;
+        String[] targetWordList = {"BONES","KHAN","KIRK","SCOTTY","SPOCK","SULU","UHURA"};
+        String[] wordList = wordSearcher.getWordList();
+        for(int index = 0; index < 7; index++) {
+            if(!wordList[index].equalsIgnoreCase(targetWordList[index])){
+                allWordsFound = false;
+            }
+        }
+
+        assertTrue(allWordsFound);
+    }
+
 }
